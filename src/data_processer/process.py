5c4d4da0421d5dae136b399cb75739ea72853d55
bekirufuk <bekirufuk@gmail.com>
2022-05-29 17:08:40 +0300

    implement dataset load and tokenization ops

---
+++ src/data_processer/process.py
@@ -0,0 +1,68 @@
+import yaml
+from transformers import LongformerTokenizerFast
+from datasets import DatasetDict, Features, Value, ClassLabel, load_dataset, load_from_disk
+
+
+def get_config():
+    config_dir = 'src/config/data.yml'
+    with open(config_dir,'r') as f:
+        config = yaml.load(f, Loader=yaml.Loader)
+    return config
+
+def get_dataset(data_name='refined_patents', seed=42, partitions={'train':0.8, 'test_validation':0.2}):
+    """Obtain the refined_patents data files as a dataset.
+
+        Parameters:
+        data_name (string): Folder name of the data to be used. Default:'refined_patents' Current options: ['refined_patents', 'sample_refined_patents']
+
+        Returns:
+        dataset: A Huggingface dataset object. https://huggingface.co/docs/datasets/access 
+        """
+    print("Loading dataset from csv...")
+    features = Features({'text': Value('string'),
+                            'labels': ClassLabel(names=["A","B","C","D","E","F","G","H"]),
+                            'ipc_class': Value('string'),
+                            'subclass': Value('string'),
+                        })
+    data_files = 'data/' + data_name + '/chunks/*.csv'
+
+    dataset = load_dataset('csv', data_files=data_files, features=features)
+    dataset = dataset['train'].train_test_split(test_size=partitions['test_validation'], shuffle=True)
+    test_val = dataset['test'].train_test_split(test_size=0.5, shuffle=True)
+
+    dataset = DatasetDict({
+        'train': dataset['train'],
+        'validation': test_val['train'],
+        'test': test_val['test']
+    })
+    return dataset
+
+def batch_tokenizer(batch, tokenizer):
+    return tokenizer(batch["text"], padding='max_length', truncation=True)
+
+def longformer_tokenizer(dataset):
+    print('Tokenizing the dataset with LongformerTokenizer')
+    tokenizer = LongformerTokenizerFast.from_pretrained('allenai/longformer-base-4096', max_length=4096)
+    tokenized_data = dataset.map(batch_tokenizer, batched=True, remove_columns=['text', 'ipc_class', 'subclass'], fn_kwargs= {"tokenizer":tokenizer})
+    tokenized_data.set_format("torch")
+    return tokenized_data
+
+def get_longformer_tokens(data_name='refined_patents', load_tokens=False, test_data_only=False):
+    if load_tokens:
+        tokenized_data = load_from_disk('data/'+data_name+'/tokenized/longformer_tokenizer/')
+    else:
+        dataset = get_dataset()
+        tokenized_data = longformer_tokenizer(dataset)
+    if test_data_only:
+        return tokenized_data['test'] 
+    else:
+        return tokenized_data['train'], tokenized_data['validation']
+
+def save_longformer_tokens(tokenized_data, data_name='refined_patents'):
+    print("Saving tokenized data...")
+    tokenized_data.save_to_disk('data/'+data_name+'/tokenized/longformer_tokenizer/')        
+
+if __name__ == '__main__':
+    dataset = get_dataset()
+    tokenized_data = longformer_tokenizer(dataset)
+    save_longformer_tokens(tokenized_data)
