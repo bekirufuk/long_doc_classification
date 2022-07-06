67d16041cc1b937ab2307f0d96244d6ba622aa44
bekirufuk <bekirufuk@gmail.com>
2022-07-06 12:30:27 +0300

    complete tfidf mapping and training process

--- src/tests/attention_map_tester.py
+++ src/tests/attention_map_tester.py
@@ -6,7 +6,6 @@
 import sys
 import os
 import yaml
-import wandb
 import pandas as pd
 from tqdm.auto import tqdm
 from datetime import datetime
@@ -45,99 +44,24 @@ if __name__ == '__main__':
     patent_ids = train_data['patent_id']
     train_data = train_data.remove_columns(['patent_id', 'ipc_class', 'subclass'])
     train_data.set_format("torch")
-
-    # Utilize the model with custom config file specifiyng classification labels.
-    print('Initializing the Longformer model...')
-    longformer_config = LongformerConfig.from_json_file('src/config/model_configs/longformer.json')
-    model = LongformerForSequenceClassification.from_pretrained('allenai/longformer-base-4096', config=longformer_config)
-    model.gradient_checkpointing_enable()
-
-    for param in model.base_model.parameters():
-        param.requires_grad = False
     
     train_dataloader = DataLoader(train_data, batch_size=finetune_config['train_batch_size'])
     #validation_dataloader = DataLoader(validation_data, batch_size=finetune_config['validation_batch_size'])
 
-    optimizer = torch.optim.AdamW(model.parameters(), lr=finetune_config['learning_rate'], weight_decay=finetune_config['weight_decay'])
 
     num_training_step = int(finetune_config['epochs'] * (finetune_config['train_sample_size'] / finetune_config['train_batch_size']))
-    num_warmup_steps = int(finetune_config['warmup_ratio'] * num_training_step)
-
-    # Learning rate scheduler for dynamic learning rate.
-    lr_scheduler = get_cosine_schedule_with_warmup(
-        optimizer=optimizer,
-        num_warmup_steps=num_warmup_steps,
-        num_training_steps=num_training_step
-    )
 
     progress_bar = tqdm(range(num_training_step))
-    log_interval = int(num_training_step/finetune_config['log_count'])
-
-    # Dict to keep track of the running avg. scores
-    train_tracker = {'running_loss':0, 'running_loss_counter':0, 'running_accuracy':0, 'running_accuracy_counter':0}
-    best_train_accuracy=0
 
     # Load the sparse tfidf matrix and the feature_names(containing input_ids as words)
     tfidf_sparse = pd.read_pickle('data/refined_patents/tfidf/longformer_tokenizer_no_stopwords/train_tfidf_sparse.pkl')
     f_names = pd.read_pickle('data/refined_patents/tfidf/longformer_tokenizer_no_stopwords/train_f_list.pkl')
 
     step_counter=0
-    model.train()
-    print("\n----------\n TRAINING STARTED \n----------\n")
     for epoch in range(finetune_config['epochs']):
         for batch_id, batch in enumerate(train_dataloader):
             # Determine the range of tfidf sparse matrix for the current batch. These document scores will match exatcly with the current batch input docs.
             tfidf_range_start = finetune_config['train_batch_size']*batch_id
             tfidf_range_end = tfidf_range_start + finetune_config['train_batch_size']
 
-            global_attention_map = map_tfidf(tfidf_sparse[tfidf_range_start:tfidf_range_end], f_names, batch['input_ids'])
-            #print(global_attention_map)
-
-            '''outputs = model(batch['input_ids'], 
-                            labels=batch['labels'],
-                            attention_map=batch['attention_map'],
-                            global_attention_map=global_attention_map
-                            )
-
-            optimizer.zero_grad()
-            accelerator.backward(outputs.loss)
-
-            optimizer.step()
-            lr_scheduler.step()
-
-            progress_bar.update(1)
-            step_counter+=1'''
-
-
-    print("\n----------\n TRAINING FINISHED \n----------\n")
-
-    """print("\n----------\n EVALUATION STARTED \n----------\n")
-    torch.cuda.empty_cache()
-
-    # Load the best model.
-    #model = LongformerForSequenceClassification.from_pretrained('models/{model_type}/{model}'.format(model_type=finetune_config['model_type'], model=finetune_config['model']))
-
-    test_data = get_longformer_tokens(test_data_only=True,load_tokens=True, test_sample_size=finetune_config['test_sample_size'])
-    test_dataloader = DataLoader(test_data, batch_size=finetune_config['test_batch_size'])
-
-    num_test_step = len(test_dataloader)
-    progress_bar = tqdm(range(num_test_step))
-
-    test_running_accuracy = 0
-
-    with torch.no_grad():
-        for batch_id, batch in enumerate(test_dataloader):
-
-            outputs = model(**batch)
-
-            predictions = torch.argmax(outputs.logits, dim=-1)
-
-            test_running_accuracy += accuracy_score(batch["labels"].cpu(), predictions.cpu())
-
-            progress_bar.update(1)
-
-    test_accuracy = test_running_accuracy / (batch_id+1)
-
-    wandb.log({"Test Accuracy":test_accuracy})
-
-    print("\n----------\n EVALUATION FINISHED \n----------\n")"""
+            global_attention_map = map_tfidf(tfidf_sparse[tfidf_range_start:tfidf_range_end], f_names, batch['input_ids'], batch['labels'])
--- src/utils/attention_mapper.py
+++ src/utils/attention_mapper.py
@@ -1,20 +1,79 @@
+'''
+    This script consist of various functions that takes a batch of 'input_ids' as input and outputs a same size matrix consisting ones and zeros.
+    Ones representing the global connection between that token (represented by tokenizers input_id) and all the others for that input document.
+    
+    Detailed explanation for the usage of global_attentions in custom transformer models can be found at the docstring of
+    'LongformerSequenceClassifierOutput' class in the HuggingFace repo: https://github.com/huggingface/transformers/blob/main/src/transformers/models/longformer/modeling_longformer.py 
+'''
+
+import numpy as np
 import pandas as pd
-from torch import zeros, float32
+from torch import zeros, long
 
-def map_tfidf(tfidf, f_names, input_ids):
+# Uncomment below code for 'tfidf_qual_analysis' function.
+'''from transformers import LongformerTokenizerFast
+tokenizer = LongformerTokenizerFast.from_pretrained('allenai/longformer-base-4096', max_length=4096)
+id2label =  {0: "A", 1: "B", 2: "C", 3: "D", 4: "E", 5: "F", 6: "G", 7: "H"}
+label2description= {'A': 'Human Necessities', 'B':'Performing Operations; Transporting', 'C':'Chemistry; Metallurgy','D':'Textiles; Paper',
+                    'E':'Fixed Constructions','F':'Mechanical Engineering; Lighting; Heating; Weapons; Blasting','G':'Physics','H':'Electricity'}'''
 
-    # Get ready the tfidf scores for the current batch
+def tfidf_qual_analysis(tfidf, f_names, input_ids, labels):
+
+    # Convert the tfidf scores as DataFrame for the current batch.
     tfidf = pd.DataFrame(tfidf.toarray(), columns=f_names)
 
-    # Put the mask in cpu temporarily for .apply_() process.
-    global_attention_mask = input_ids.type(float32).cpu()
+    # Assingn zero to some specific columns of tfidf matrix. These are the column names for the following tokens: ['[PAD]', '.', ',', '...' ].
+    tfidf.loc[:,['1', '4', '6', '38917']] = 0
 
-    for i, item in enumerate(global_attention_mask): # For every document(item) in the batch.
+    # Turn GPU Tensor input_ids to CPU DataFrame of strings, so the input_id values could be matched with tfidf column names (which are the same as input_ids).
+    input_ids = pd.DataFrame(input_ids.cpu().detach().numpy(), dtype=str)
+    
+    for i in range(input_ids.shape[0]): # Iterate over the batch
 
-        # x is 1 for padding tokens, so it would be zero, all other tokens matches to their tfidf scores.
-        #i represents the document id (from zero up to batch size) and the x represents the input_id, being the column name of the tfidf DataFrame.
-        globals = item.apply_(lambda x: 0 if x == 1 else tfidf.iloc[i,int(x)])
-        print(globals)
+        # From the tfidf matrix, obtain the values from row i and the colums that matches the corresponding documents input_ids.
+        # Column names of the tfidf matrix are the same with the all input_ids, we just select the ones that match the current documents input_ids.
+        tf_idf_values_of_input = tfidf.loc[i, input_ids.loc[i]]
+
+        # Cast the tfidf values to numpy array and obtain the indices of a number of highest tfidf scores.
+        # These indices correspond the indices of 'to be globally connected tokens' for the model forward. 
+        tf_idf_values_of_input = np.array(tf_idf_values_of_input, dtype=np.float16)
+        top_tfidf_idxs = np.argsort(tf_idf_values_of_input)[-128:]
+
+        # Uncomment below code for a detailed view of the selected input_ids.
+        '''print('sorted scores: ', np.sort(tf_idf_values_of_input,)[-128:])
+        print('argsort idxs: ',top_tfidf_idxs)
+        print('argsort values: ',tf_idf_values_of_input[top_tfidf_idxs])
+        print('input ids: ', list(input_ids.loc[i][top_tfidf_idxs]))'''
+        
+        # Print the label description of the document along with the determinded global tokens.
+        print(label2description[id2label[labels[i].item()]],':')
+        print(tokenizer.convert_ids_to_tokens(list(input_ids.loc[i][top_tfidf_idxs])))
         print('\n################\n')
 
-    return tfidf
+def map_tfidf(tfidf, f_names, input_ids, device):
+
+    # Convert the tfidf scores as DataFrame for the current batch.
+    tfidf = pd.DataFrame(tfidf.toarray(), columns=f_names)
+
+    # Assingn zero to some specific columns of tfidf matrix. These are the column names for the following tokens: ['[PAD]', '.', ',', '...' ].
+    tfidf.loc[:,['1', '4', '6', '38917']] = 0
+
+    # Turn GPU Tensor input_ids to CPU DataFrame of strings, so the input_id values could be matched with tfidf column names (which are the same as input_ids).
+    input_ids = pd.DataFrame(input_ids.cpu().detach().numpy(), dtype=str)
+
+    # Initilize an empty global_attention_map with the same size as the input.
+    global_attention_map = zeros(input_ids.shape, dtype=long, device=device)
+    
+    for i in range(input_ids.shape[0]): # For every document(item) in the batch.
+
+        # Cast the tfidf values to numpy array and obtain the indices of a number of highest tfidf scores.
+        # These indices correspond the indices of 'to be globally connected tokens' for the model forward. 
+        tf_idf_values_of_input = np.array(tf_idf_values_of_input, dtype=np.float16)
+        top_tfidf_idxs = np.argsort(tf_idf_values_of_input)[-128:]
+
+        # Assign the value one to the positions of determined global tokens.
+        global_attention_map[i][top_tfidf_idxs] = 1
+
+    # Assign one to the first token of every document since it is the classification ([CLS]) token and should always be globally connected.
+    global_attention_map[:, 0] = 1  
+    return global_attention_map
