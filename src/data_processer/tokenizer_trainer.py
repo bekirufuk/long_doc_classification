584b4771f154fc7b3bf02d4703fbf3e407894011
bekirufuk <bekirufuk@gmail.com>
2022-10-28 16:40:16 +0300

    remove custom data_loader

--- src/data_processer/tokenizer_trainer.py
+++ src/data_processer/tokenizer_trainer.py
@@ -1,33 +1,10 @@
 import sys, os
 
-from transformers import AutoTokenizer, BertTokenizerFast
-from datasets import DatasetDict, Features, Value, ClassLabel, load_dataset, load_from_disk
+from transformers import BertTokenizerFast
 
-#sys.path.append(os.getcwd())
-#from src.data_processer.process import get_dataset
+sys.path.append(os.getcwd())
+from src.data_processer.process import get_dataset
 
-
-def get_dataset(data_name='refined_patents', seed=42, partitions={'train':0.8, 'test_validation':0.2}):
-    print("Loading dataset from csv...")
-    features = Features({   'patent_id': Value('string'),
-                            'text': Value('string'),
-                            'labels': ClassLabel(names=["A","B","C","D","E","F","G","H"]),
-                            'ipc_class': Value('string'),
-                            'subclass': Value('string'),
-                        })
-                        
-    data_files = 'data/'+data_name+'/chunks/*.csv' # add preprocess forlder
-
-    dataset = load_dataset('csv', data_files=data_files, features=features, cache_dir='data/'+data_name+'/cache')
-    dataset = dataset['train'].train_test_split(test_size=partitions['test_validation'], shuffle=True, seed=seed)
-    test_val = dataset['test'].train_test_split(test_size=0.5, shuffle=True, seed=seed)
-
-    dataset = DatasetDict({
-        'train': dataset['train'],
-        'validation': test_val['train'],
-        'test': test_val['test']
-    })
-    return dataset
 
 def get_training_corpus(dataset):
     for start_idx in range(0, len(dataset), 1000):
